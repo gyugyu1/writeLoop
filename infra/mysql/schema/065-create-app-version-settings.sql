3dac9469c7753ffa7785a6b50c0b5636b2ce7df5
gyuwon <lwd33021@naver.com>
2026-05-30 22:30:37 +0900

    앱스토어 한국 링크를 바로잡다

--- infra/mysql/schema/065-create-app-version-settings.sql
+++ infra/mysql/schema/065-create-app-version-settings.sql
@@ -30,7 +30,7 @@ INSERT INTO app_version_settings (
         'ios',
         '1.0.2',
         '1.0.0',
-        'https://apps.apple.com/app/id6763569959',
+        'https://apps.apple.com/kr/app/%EB%9D%BC%EC%9D%B4%ED%8A%B8%EB%A3%A8%ED%94%84/id6763569959',
         '새 버전이 나왔어요',
         '업데이트가 필요해요',
         '더 안정적인 학습 경험을 위해 최신 버전으로 업데이트해 주세요.',
---
+++ infra/mysql/schema/082-fix-ios-app-store-korea-url.sql
@@ -0,0 +1,3 @@
+UPDATE app_version_settings
+SET store_url = 'https://apps.apple.com/kr/app/%EB%9D%BC%EC%9D%B4%ED%8A%B8%EB%A3%A8%ED%94%84/id6763569959'
+WHERE platform = 'ios';
